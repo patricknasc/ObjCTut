0a3a224f77d16a1bbb18ccfd54f1c056849d2034
Patrick Nascimento <patrick.nascimento@gmail.com>
2016-08-31 19:19:13 -0300

    Delete 2fahrenheitCelsiusConverter.c

--- ObjCTut/2fahrenheitCelsiusConverter.c
+++
@@ -1,9 +0,0 @@
-//
-//  2fahrenheitCelsiusConverter.c
-//  ObjCTut
-//
-//  Created by Patrick Nascimento on 31/08/16.
-//  Copyright © 2016 patrix. All rights reserved.
-//
-
-#include "2fahrenheitCelsiusConverter.h"
