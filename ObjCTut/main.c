30627cf7d0bda7760e3b523fd075b640706427bc
patricknasc <patrick.nascimento@gmail.com>
2016-08-31 03:05:29 -0300

    Initial Commit

---
+++ ObjCTut/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ObjCTut
+//
+//  Created by Patrick Nascimento on 31/08/16.
+//  Copyright © 2016 patrix. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
