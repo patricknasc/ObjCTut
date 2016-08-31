4ad509fce8f59035f580ea75f4f5681a490ee278
patricknasc <patrick.nascimento@gmail.com>
2016-08-31 11:04:46 -0300

    [C Language] Study series, passing arguments to main function and printing it.

--- ObjCTut/main.c
+++ ObjCTut/main.c
@@ -9,7 +9,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    for (int i=0; i< argc; i++) {
+        printf("arg %d : %s\n", i, argv[i]);
+    }
+    
+    
     return 0;
 }
